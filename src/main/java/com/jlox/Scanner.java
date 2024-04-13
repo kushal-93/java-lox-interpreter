d270c62781e8e46dded2cbece6a8e58b5ddaac1e
kushal-93 <kushalmondal93@gmail.com>
2024-04-13 20:41:42 +0530

    read source lines

---
+++ src/main/java/com/jlox/Scanner.java
@@ -0,0 +1,13 @@
+package com.jlox;
+
+import java.util.List;
+
+public class Scanner {
+    Scanner(String source) {
+
+    }
+
+    public List<Token> scanTokens() {
+        return null;
+    }
+}
---
+++ src/main/java/com/jlox/Token.java
@@ -0,0 +1,5 @@
+package com.jlox;
+
+public class Token {
+    
+}

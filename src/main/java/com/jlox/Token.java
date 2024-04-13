0d6b74af09017225c7402a31f0c0404094a97865
kushal-93 <kushalmondal93@gmail.com>
2024-04-13 21:24:06 +0530

    add token setup

--- src/main/java/com/jlox/Token.java
+++ src/main/java/com/jlox/Token.java
@@ -2,4 +2,22 @@ package com.jlox;
 
 public class Token {
     
+    private final TokenType type;
+    private final String lexeme;
+    private final int line;
+    private final int column;
+    private final Object literal;
+
+    Token(TokenType type, String lexeme, int line, int column, Object literal) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.column = column;
+        this.literal = literal;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal + " [" + line + ", " + column +"]"; 
+    }
+
 }

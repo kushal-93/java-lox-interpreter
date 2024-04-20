20d3cdf6bac3bc7ae072211eb9930bde176f11de
kushal-93 <kushalmondal93@gmail.com>
2024-04-20 19:09:22 +0530

    read identifiers

--- src/main/java/com/jlox/Scanner.java
+++ src/main/java/com/jlox/Scanner.java
@@ -1,9 +1,33 @@
 package com.jlox;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 
+
 public class Scanner {
+
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+      }
 
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -119,8 +143,33 @@ public class Scanner {
         return true;
     }
 
+    private void identifier() {
+        char ch = peek();
+        while(isAlphaNumeric(ch)) {
+            advance();
+            ch = peek();
+        }
+            
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null)
+            type = TokenType.IDENTIFIER;
+        addToken(type);
+    }
+
     private boolean isDigit(char c) {
         return (c >= '0' && c <= '9');
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') 
+            || (c >= 'A' && c <= 'Z') 
+            || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isDigit(c) || isAlpha(c);
     }
 
     private void scanToken() {
@@ -143,8 +192,7 @@ public class Scanner {
                 if(isDigit(peek())) {
                     if(number(true))
                         addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
-                }
-                    
+                }                    
                 else
                     addToken(TokenType.DOT); 
                 break;
@@ -203,6 +251,8 @@ public class Scanner {
                 if(isDigit(c)) {
                     if(number(false))
                         addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+                } else if (isAlpha(c)) {
+                    identifier();
                 }
                 else {
                     ErrorHandler.error(line, start+1, "Unexpected character!");
